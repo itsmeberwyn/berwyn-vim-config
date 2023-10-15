3fec31f27e56c148d9158f0de6c77300f87a9c0b
Berwyn Felismenia <59910047+itsmeberwyn@users.noreply.github.com>
2023-10-15 22:41:57 +0800

    :sparkle: vim config (lua, packer)

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("berwyn.plugins-setup")
+require("berwyn.core.options")
+require("berwyn.core.keymaps")
+require("berwyn.core.colorscheme")
+require("berwyn.plugins.comment")
+require("berwyn.plugins.nvim-tree")
+require("berwyn.plugins.lualine")
+require("berwyn.plugins.telescope")
+
+
---
+++ lua/berwyn/plugins-setup.lua
@@ -0,0 +1,81 @@
+-- auto install packer if not installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- import packer safely
+local status, packer = pcall(require, "packer")
+if not status then
+  return
+end
+
+return packer.startup(function(use)
+  use('wbthomason/packer.nvim')
+
+  use("nvim-lua/plenary.nvim")
+
+  -- for colorscheme
+  use("bluz71/vim-nightfly-guicolors")
+
+  -- tmux and split window navigation
+  use("christoomey/vim-tmux-navigator")
+
+  -- maximizes and restores current window
+  use("szw/vim-maximizer")
+
+  -- essential plugsin
+  -- ysw" <- to surround with "
+  -- ds" <- to remove "
+  -- cs"' <- to change " to '
+  use("tpope/vim-surround")
+
+  -- replace word of what you currently copied
+  -- grw to replace word
+  use("vim-scripts/ReplaceWithRegister")
+
+  -- commenting with gc
+  -- need configuration in plugins folder
+  -- 'gcc' to comment
+  -- 'gc<int><h,j,k,l> to comment specific number of lines
+  use("numToStr/Comment.nvim")
+
+  -- file explorer
+  -- press 'a' to create file while in explorer
+  use("nvim-tree/nvim-tree.lua")
+
+  -- icons for nvim tree
+  use("kyazdani42/nvim-web-devicons")
+
+  -- statusline
+  use("nvim-lualine/lualine.nvim")
+
+  -- fuzzy finding w/ telescope
+  -- possible error you may encounter when you are using window to setup this plugin
+  -- install theses two plugin to solve problem installing telescope
+  -- https://github.com/sharkdp/fd
+  -- https://github.com/BurntSushi/ripgrep
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
+end)
+
