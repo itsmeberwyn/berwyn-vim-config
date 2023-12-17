65430ced5ac38ecf1373edca86cbe034f08d9bd6
Berwyn Felismenia <59910047+itsmeberwyn@users.noreply.github.com>
2023-12-17 19:12:53 +0800

    :art: comment open mapping on toggle term, conflict on <C-j> navigation window

--- lua/berwyn_v2/plugins/toggleterm.lua
+++ lua/berwyn_v2/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			local toggleterm = require("toggleterm")
 			toggleterm.setup({
-				open_mapping = [[<c-j>]],
+				-- open_mapping = [[<c-j>]],
 				size = 20,
 				start_in_insert = false,
 				close_on_exit = true,
